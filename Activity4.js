16fbb984903f8b2e0cc82b02cc290b1085f615a7
Dazel123 <dazelcatabuena719@gmail.com>
2023-12-15 21:36:32 -0800

    Done

---
+++ Activity4.js
@@ -0,0 +1,11 @@
+let firstName = "Jeanah";
+let lastName = "Carezo";
+let $age = 20;
+let civil_status = "Single";
+let home_address = "Sta Remedios, Bulan Sorsogon";
+let $Univ_name = "Sorsogon State University";
+let $YEAR = "2";
+let Crush = "joe";
+let student_ID = "21-29382";
+let $phoneNumber = "09832323739"
+
